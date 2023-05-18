e2c4f921f072f27ba5f8a80c628b6275d7d38b8c
LordAkuos <filipeprimodeoliveira@gmail.com>
2023-05-18 20:20:43 -0300

    Horário-estilo7

--- projeto/interface_siga/resources/views/site/horario.blade.php
+++ projeto/interface_siga/resources/views/site/horario.blade.php
@@ -11,215 +11,211 @@ Iniciar
         <button id="prof"><i></i>Conheça nossos professores</button>
         <br>
         <div class="row">
-            <div class="col">
-                <div class="col-sm-4 col-md-6 col-lg-12">
-                    <table>
-                        <thead>
-                        <tr>Segunda-feira</tr> 
-                        </thead>
-                        <tbody>
-                            <tr class="top-table">
-                                <th>Horário</th>
-                                <th>Disciplina</th>
-                                <th>Turma</th>
-                            </tr>
-                            <tr>
-                                <td>19:00 - 19:50</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>19:50 - 20:40</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>20:50 - 21:40</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>21:40 - 22:30</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-sm-4 col-md-6 col-lg-12">
+            <div class="col-sm-4 col-md-6 col-lg-12">
                 <table>
-                        <thead>
-                        <tr>Terça-feira</tr> 
-                        </thead>
-                        <tbody>
-                            <tr class="top-table">
-                                <th>Horário</th>
-                                <th>Disciplina</th>
-                                <th>Turma</th>
-                            </tr>
-                            <tr>
-                                <td>19:00 - 19:50</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>19:50 - 20:40</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>20:50 - 21:40</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>21:40 - 22:30</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-sm-4 col-md-6 col-lg-12">
+                    <thead>
+                    <tr>Segunda-feira</tr> 
+                    </thead>
+                    <tbody>
+                        <tr class="top-table">
+                            <th>Horário</th>
+                            <th>Disciplina</th>
+                            <th>Turma</th>
+                        </tr>
+                        <tr>
+                            <td>19:00 - 19:50</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>19:50 - 20:40</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>20:50 - 21:40</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>21:40 - 22:30</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-4 col-md-6 col-lg-12">
                 <table>
-                        <thead>
-                        <tr class="top-table">Quarta-feira</tr> 
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>Horário</th>
-                                <th>Disciplina</th>
-                                <th>Turma</th>
-                            </tr>
-                            <tr>
-                                <td>19:00 - 19:50</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>19:50 - 20:40</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>20:50 - 21:40</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>21:40 - 22:30</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                    <thead>
+                    <tr>Terça-feira</tr> 
+                    </thead>
+                    <tbody>
+                        <tr class="top-table">
+                            <th>Horário</th>
+                            <th>Disciplina</th>
+                            <th>Turma</th>
+                        </tr>
+                        <tr>
+                            <td>19:00 - 19:50</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>19:50 - 20:40</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>20:50 - 21:40</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>21:40 - 22:30</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-4 col-md-6 col-lg-12">
+                <table>
+                    <thead>
+                    <tr class="top-table">Quarta-feira</tr> 
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Horário</th>
+                            <th>Disciplina</th>
+                            <th>Turma</th>
+                        </tr>
+                        <tr>
+                            <td>19:00 - 19:50</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>19:50 - 20:40</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>20:50 - 21:40</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>21:40 - 22:30</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
         <div class="row">
-            <div class="col">
-                <div class="col-sm-4 col-md-6 col-lg-12">
+            <div class="col-sm-4 col-md-6 col-lg-12">
                 <table>
-                        <thead>
-                        <tr>Quinta-feira</tr> 
-                        </thead>
-                        <tbody>
-                            <tr class="top-table">
-                                <th>Horário</th>
-                                <th>Disciplina</th>
-                                <th>Turma</th>
-                            </tr>
-                            <tr>
-                                <td>19:00 - 19:50</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>19:50 - 20:40</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>20:50 - 21:40</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>21:40 - 22:30</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-sm-4 col-md-6 col-lg-12">
+                    <thead>
+                    <tr>Quinta-feira</tr> 
+                    </thead>
+                    <tbody>
+                        <tr class="top-table">
+                            <th>Horário</th>
+                            <th>Disciplina</th>
+                            <th>Turma</th>
+                        </tr>
+                        <tr>
+                            <td>19:00 - 19:50</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>19:50 - 20:40</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>20:50 - 21:40</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>21:40 - 22:30</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-4 col-md-6 col-lg-12">
                 <table class="horario">
-                        <thead>
+                    <thead>
                         <tr>Sexta-feira</tr> 
-                        </thead>
-                        <tbody>
-                            <tr class="top-table">
-                                <th>Horário</th>
-                                <th>Disciplina</th>
-                                <th>Turma</th>
-                            </tr>
-                            <tr>
-                                <td>19:00 - 19:50</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>19:50 - 20:40</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>20:50 - 21:40</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>21:40 - 22:30</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-sm-4 col-md-6 col-lg-12">
-                    <table>
-                        <thead>
-                            <tr>Sábado</tr> 
-                        </thead>
-                        <tbody>
-                            <tr class="top-table">
-                                <th>Horário</th>
-                                <th>Disciplina</th>
-                                <th>Turma</th>
-                            </tr>
-                            <tr>
-                                <td>07:40 - 08:30</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>08:30 - 09:20</td>
-                                <td>IRC100</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>09:30 - 10:20</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                            <tr>
-                                <td>10:20 - 11:10</td>
-                                <td>AG0005</td>
-                                <td>A</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                    </thead>
+                    <tbody>
+                        <tr class="top-table">
+                            <th>Horário</th>
+                            <th>Disciplina</th>
+                            <th>Turma</th>
+                        </tr>
+                        <tr>
+                            <td>19:00 - 19:50</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>19:50 - 20:40</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>20:50 - 21:40</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>21:40 - 22:30</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-4 col-md-6 col-lg-12">
+                <table>
+                    <thead>
+                        <tr>Sábado</tr> 
+                    </thead>
+                    <tbody>
+                        <tr class="top-table">
+                            <th>Horário</th>
+                            <th>Disciplina</th>
+                            <th>Turma</th>
+                        </tr>
+                        <tr>
+                            <td>07:40 - 08:30</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>08:30 - 09:20</td>
+                            <td>IRC100</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>09:30 - 10:20</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                        <tr>
+                            <td>10:20 - 11:10</td>
+                            <td>AG0005</td>
+                            <td>A</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
