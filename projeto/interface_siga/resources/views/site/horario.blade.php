11858b875c0c43c39685ffbda8f24769ed8f46b8
LordAkuos <filipeprimodeoliveira@gmail.com>
2023-05-18 19:11:02 -0300

    Update horario.blade.php

--- projeto/interface_siga/resources/views/site/horario.blade.php
+++ projeto/interface_siga/resources/views/site/horario.blade.php
@@ -195,22 +195,22 @@ Iniciar
                             <th>Turma</th>
                         </tr>
                         <tr>
-                            <td>19:00 - 19:50</td>
+                            <td>07:40 - 08:30</td>
                             <td>IRC100</td>
                             <td>A</td>
                         </tr>
                         <tr>
-                            <td>19:50 - 20:40</td>
+                            <td>08:30 - 09:20</td>
                             <td>IRC100</td>
                             <td>A</td>
                         </tr>
                         <tr>
-                            <td>20:50 - 21:40</td>
+                            <td>09:30 - 10:20</td>
                             <td>AG0005</td>
                             <td>A</td>
                         </tr>
                         <tr>
-                            <td>21:40 - 22:30</td>
+                            <td>10:20 - 11:10</td>
                             <td>AG0005</td>
                             <td>A</td>
                         </tr>
