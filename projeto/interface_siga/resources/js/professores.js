3b4741ded05b80c067dc8ef6fea86bf2e6e590bd
Michael <filipeprimodeoliveira@gmail.com>
2023-05-29 21:56:10 -0300

    professor

--- projeto/interface_siga/resources/js/professores.js
+++ projeto/interface_siga/resources/js/professores.js
@@ -1,10 +1,14 @@
 let botao = document.getElementById('prof');
+let telaProf = document.getElementById('professores');
 
-botao.addEventListener('click', function() {
-    let telaProf = document.getElementById('professores');
-    if(telaProf.style.display === "none"){
+botao.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    let professor = telaProf.nextElementSibling;
+    professor.classList.toggle('ativo');
+    /*if(telaProf.style.display === "none"){
         telaProf.style.display = "block";
     }else{
         telaProf.style.display = "none";
-    }
+    }*/
 });
