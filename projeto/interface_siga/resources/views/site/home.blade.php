eda0d4ceb336dac1915899f369144af00b2685df
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2023-06-11 19:38:53 -0300

    AB#13 Tela inicial

--- projeto/interface_siga/resources/views/site/home.blade.php
+++ projeto/interface_siga/resources/views/site/home.blade.php
@@ -8,7 +8,15 @@ Home
 
 <h2>Home</h2>
 <div class="conteudo-principal">
-    <p>teste</p>
+    <div class="aviso-erro">Prazo para encerramento da matricula até dia 01/02</div>
+    <h3 class="home-noticias">Ultimas Noticias</h3>
+
+    <div class="home-banner">
+        <img src="{{ Vite::asset('resources/images/banner1 (1).jpg') }}" alt="Person" width="360" height="450">
+        <img src="{{ Vite::asset('resources/images/banner1 (2).jpg') }}" alt="Person" width="360" height="450">
+    </div>
+
+   
               
 </div>
 
