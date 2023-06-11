08c484ae2a8aa38e43cabc2e9aefe7babfcacca0
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2023-06-11 19:51:34 -0300

    AB#13 Tela inicial1

--- projeto/interface_siga/resources/views/site/home.blade.php
+++ projeto/interface_siga/resources/views/site/home.blade.php
@@ -12,8 +12,16 @@ Home
     <h3 class="home-noticias">Ultimas Noticias</h3>
 
     <div class="home-banner">
+        <p>ATENÇÃO ESTUDANDE</p>
+        <p>Está buscando conexão?</p>
         <img src="{{ Vite::asset('resources/images/banner1 (1).jpg') }}" alt="Person" width="360" height="450">
         <img src="{{ Vite::asset('resources/images/banner1 (2).jpg') }}" alt="Person" width="360" height="450">
+    </div>
+
+    <div class="home-banner">
+        <p>O Geek Fest esse ano vem com várias novidades...</p>
+        <p>Dias 23 e 24 de setembro de 2023</p>
+        <img src="{{ Vite::asset('resources/images/banner3.png') }}" alt="Person" width="450" height="450">
     </div>
 
    
